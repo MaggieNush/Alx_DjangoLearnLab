996225212e356f69381a68d04974ca1ef6c984ea
MaggieNush <maggienungari.mn@gmail.com>
2025-07-20 22:33:18 +0300

    Add new permission files

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/registration/logged_out.html'), name='logout'),
-    
+
     path('admin/', views.admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
