0595324079c84af3291504179a4c43666f26820f
MaggieNush <maggienungari.mn@gmail.com>
2025-07-20 19:27:35 +0300

    Add a new app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,9 @@
 # relationship_app/views.py
-from django.shortcuts import render, redirect, get_object_or_404 # Ensure 'redirect' is imported
-from django.views.generic import DetailView
-from .models import Book, Library
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView 
+from .models import Book, Library 
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages # Needed for messages.success
 
 # --- Function-based View: List All Books ---
 def all_books_list_view(request):
@@ -11,12 +11,18 @@ def all_books_list_view(request):
     A function-based view to display a comprehensive list of all books
     available in the library system, along with their authors.
     """
+    # Fetch all Book objects from the database.
+    # .select_related('author') is an optimization to fetch author data
+    # in the same database query, preventing N+1 query problems.
     all_books = Book.objects.all().select_related('author').order_by('title')
+
+    # Prepare the context dictionary to pass data to the template.
     context = {
         'books': all_books,
         'page_title': 'Our Complete Book Collection',
         'intro_message': 'Explore every title we have at a glance.'
     }
+    # Render the list_books.html template with the prepared context.
     return render(request, 'relationship_app/list_books.html', context)
 
 
@@ -27,32 +33,65 @@ class LibraryDetailView(DetailView):
     of a specific library, including all books it contains.
     This view is designed to be accessed via a primary key (pk) in the URL.
     """
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+    model = Library # Specify the model this view will operate on.
+    template_name = 'relationship_app/library_detail.html' # Path to your template.
+    context_object_name = 'library' # The variable name to use for the Library object in the template.
+
+    # You can customize the queryset if needed, but for a simple detail, model = Library is enough.
+    # def get_queryset(self):
+    #     # Example: If you wanted to prefetch books for every library detail view
+    #     return Library.objects.all().prefetch_related('books__author')
+
+    # get_context_data is optional, used if you need to add extra context beyond the single object.
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add any extra context specific to a library's details page
+        context['current_year'] = 2025 # Just an example of extra context
+        return context# relationship_app/views.py
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView # Correctly using DetailView for a specific item
+from .models import Book, Library # Import your models
+
+# --- Function-based View: List All Books ---
+def all_books_list_view(request):
+    """
+    A function-based view to display a comprehensive list of all books
+    available in the library system, along with their authors.
+    """
+    # Fetch all Book objects from the database.
+    # .select_related('author') is an optimization to fetch author data
+    # in the same database query, preventing N+1 query problems.
+    all_books = Book.objects.all().select_related('author').order_by('title')
+
+    # Prepare the context dictionary to pass data to the template.
+    context = {
+        'books': all_books,
+        'page_title': 'Our Complete Book Collection',
+        'intro_message': 'Explore every title we have at a glance.'
+    }
+    # Render the list_books.html template with the prepared context.
+    return render(request, 'relationship_app/list_books.html', context)
+
+
+# --- Class-based View: Library Details ---
+class LibraryDetailView(DetailView):
+    """
+    A class-based view (using Django's DetailView) to display the details
+    of a specific library, including all books it contains.
+    This view is designed to be accessed via a primary key (pk) in the URL.
+    """
+    model = Library # Specify the model this view will operate on.
+    template_name = 'relationship_app/library_detail.html' # Path to your template.
+    context_object_name = 'library' # The variable name to use for the Library object in the template.
+
+    # You can customize the queryset if needed, but for a simple detail, model = Library is enough.
+    # def get_queryset(self):
+    #     # Example: If you wanted to prefetch books for every library detail view
+    #     return Library.objects.all().prefetch_related('books__author')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['current_year'] = 2025
+        # Add any extra context specific to a library's details page
+        context['current_year'] = 2025 # Just an example of extra context
         return context
-
-# --- ADD THIS ENTIRE FUNCTION BLOCK ---
-def register(request):
-    """
-    Handles user registration.
-    This view processes the UserCreationForm for new user sign-ups.
-    """
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}! You can now log in.')
-            return redirect('relationship_app:login') # Redirect to the login page defined in urls.py
-        # If the form is not valid, it will fall through and render with errors
-    else:
-        # This is the UserCreationForm() the checker is looking for on a GET request
-        form = UserCreationForm()
-
-    # This is the 'relationship_app/register.html' template render the checker is looking for
-    return render(request, 'relationship_app/registration/register.html', {'form': form})
+    
