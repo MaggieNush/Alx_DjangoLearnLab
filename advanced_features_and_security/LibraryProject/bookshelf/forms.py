c5a94961590c1085218b59df2ad4d4e45498f286
MaggieNush <maggienungari.mn@gmail.com>
2025-07-27 02:33:16 +0300

    Add security features

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,7 +1,5 @@
 from django import forms
-from .models import Book
 
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title', 'author']
+class ExampleForm(forms.Form):
+    """Simple example form"""
+    example_field = forms.CharField(label='Example Field')
