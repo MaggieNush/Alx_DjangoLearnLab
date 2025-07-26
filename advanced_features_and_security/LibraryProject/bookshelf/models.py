bbd22d279178a721ba4e25ba7e9e624de2adec79
MaggieNush <maggienungari.mn@gmail.com>
2025-07-26 21:12:53 +0300

    Add custom user manager

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
 class Book (models.Model):
@@ -6,3 +8,34 @@ class Book (models.Model):
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
 
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField
+    profile_photo = models.ImageField
+
+class CustomManagerModel(BaseUserManager):
+    def create_user(self, date_of_birth, profile_photo, **extra_fields):
+        """
+        create and save users with the given DOB and profile photo
+        """
+        if not date_of_birth:
+            raise ValueError("The date of birth must be set")
+        date_of_birth = self.date_of_birth(date_of_birth)
+        user = self.model(date_of_birth=date_of_birth, **extra_fields)
+        user.set_profile_photo(profile_photo)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, date_of_birth, profile_photo, **extra_fields):
+        """
+        create and save users with the given DOB and profile photo
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is staff=True.")
+        
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(date_of_birth, profile_photo, **extra_fields)
