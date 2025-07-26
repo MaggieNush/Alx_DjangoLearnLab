09643d05126690d48a6a24c4107f7d28a6cd4e13
MaggieNush <maggienungari.mn@gmail.com>
2025-07-26 21:35:37 +0300

    Add custom admin manager

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -2,38 +2,56 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.models import BaseUserManager
+
 
 # Create your models here.
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField
-    profile_photo = models.ImageField
-
-class CustomManagerModel(BaseUserManager):
-    def create_user(self, date_of_birth, profile_photo, **extra_fields):
-        """
-        create and save users with the given DOB and profile photo
-        """
-        if not date_of_birth:
-            raise ValueError("The date of birth must be set")
-        date_of_birth = self.date_of_birth(date_of_birth)
-        user = self.model(date_of_birth=date_of_birth, **extra_fields)
-        user.set_profile_photo(profile_photo)
-        user.save(using=self._db)
-        return user
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
     
-    def create_superuser(self, date_of_birth, profile_photo, **extra_fields):
-        """
-        create and save users with the given DOB and profile photo
-        """
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Member')
 
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is staff=True.")
-        
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
 
-        return self.create_user(date_of_birth, profile_photo, **extra_fields)
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    if hasattr(instance, 'userprofile'):
+        instance.userprofile.save()
+
+class Author(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+    
+    def __str__(self):
+        return self.title
+
+class Library(models.Model):
+    name = models.CharField(max_length=20)
+    books = models.ManyToManyField(Book)
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=20)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
