fe43e15a4951976fbabf19e0ba0ace5bef6841c8
MaggieNush <maggienungari.mn@gmail.com>
2025-08-02 21:05:38 +0300

    Build the first API endpoint

---
+++ api_project/api/urls.py
@@ -0,0 +1,7 @@
+from rest_framework import generics
+from .serializers import BookSerializer
+from .models import Book
+
+class BookListCreateAPIView(generics.ListAPIView):
+    queryset=Book.objects.all()
+    serializer_class=BookSerializer
