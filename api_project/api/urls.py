a0d123257aa49e1a7b02e3f4ff586c497e9a630b
MaggieNush <maggienungari.mn@gmail.com>
2025-08-02 22:01:14 +0300

    Test CRUD operations

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
-from api.views import BookList
+from django.urls import path, include
+from api.views import BookList, BookViewSet
+from rest_framework.routers import DefaultRouter
 
+router=DefaultRouter()
+router.register(r'books', BookViewSet, basename='book')
 urlpatterns = [
     path('books/', BookList.as_view(),
 name="book_list_view"),
+    router.urls
 ]
