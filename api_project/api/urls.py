feff4d2f511a4ad3a818d07ede6d2e00f70767bb
MaggieNush <maggienungari.mn@gmail.com>
2025-08-02 21:19:59 +0300

    Build the first API endpoint

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 from api.views import BookListCreateAPIView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/books/', BookListCreateAPIView.as_view(),
-name="book_list_view")
+    path('books/', BookListCreateAPIView.as_view(),
+name="book_list_view"),
 ]
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from api.views import BookListCreateAPIView
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/books/', BookListCreateAPIView.as_view(),
-name="book_list_view")
+    path('api/', include('api.urls'))
 ]
