a863228d898f358c359cc98767f53f2caa5c66e8
MaggieNush <maggienungari.mn@gmail.com>
2025-08-05 13:05:54 +0300

    Build custom and generic views in Django restframeworks

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import BookSerializer
+from .models import Book
 
-# Create your views here.
+
+class BookListView(generics.ListAPIView):
+    """
+    View to retrieve all books.
+    This view uses the BookSerializer to serialize the book data.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(generics.RetrieveAPIView):
+    """
+    View to retrieve a single book by its ID.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookCreateView(generics.CreateAPIView):
+    """
+    View to create a new model instance.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookUpdateView(generics.UpdateAPIView):
+    """
+    View to update an existing book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDeleteView(generics.DestroyAPIView):
+    """
+    View to remove a book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
