006eb2bd458ca1d944e626aa918083d975c57b35
MaggieNush <maggienungari.mn@gmail.com>
2025-08-17 16:23:51 +0300

    Implements Advanced Features ie tagging

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
+# Ensures that posts and comments can be managed through the admin interface
 admin.site.register(Post)
+admin.site.register(Comment)
