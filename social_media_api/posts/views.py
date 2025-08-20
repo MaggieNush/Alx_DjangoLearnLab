599adaa6f881eb4a122dfad0277ebb56aa1d26fc
MaggieNush <maggienungari.mn@gmail.com>
2025-08-20 17:21:28 +0300

    Implement User Follows and Feed Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -72,7 +72,7 @@ class LikeToggleView(APIView):
         user = request.user
         
         # Check if the user has already liked the post.
-        like, created = Like.objects.get_or_create(post=post, user=request.user)
+        like, created = Like.objects.get_or_create(user=request.user, post=post)
 
         if created:
             # If a new like was created, send a notification.
